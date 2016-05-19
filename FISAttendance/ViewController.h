5db39c9650ce922a5e9018ed24a3aeeb24fa7672
aabato <angelica.bato@gmail.com>
2016-05-19 17:24:04 -0400

    Add project files and gitignore

---
+++ FISAttendance/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FISAttendance
+//
+//  Created by Angelica Bato on 5/19/16.
+//  Copyright © 2016 Angelica Bato. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ FISAttendance/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  FISAttendance
+//
+//  Created by Angelica Bato on 5/19/16.
+//  Copyright © 2016 Angelica Bato. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
